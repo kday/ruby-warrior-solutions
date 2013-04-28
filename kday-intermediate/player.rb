a83d3264d1ecf62d99831cedac5dfbe354bb323f
Kevin Day <kday@sparkbase.com>
2013-04-27 20:06:27 -0400

    Remove state from Player.

--- kday-intermediate/player.rb
+++ kday-intermediate/player.rb
@@ -2,15 +2,7 @@ require_relative 'scenario'
 require_relative 'brain'
 
 class Player
-  attr_accessor :brain
-
-  def initialize
-    self.brain = Brain.new()
-  end
-
   def play_turn(warrior)
-    scenario = Scenario.build(warrior)
-    action = self.brain.decide(scenario)
-    action.perform(warrior)
+    Brain.decide(Scenario.build(warrior)).perform(warrior)
   end
 end
